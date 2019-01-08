4e5ea3f66f9ec8fccd01f9f1b04bba1c012fe672
Octavio Torres <octa022@gmail.com>
2019-01-08 17:18:59 -0400

    Despliegue DHTML

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,64 @@
 <template>
   <v-app>
-    <v-toolbar app>
+    <!-- Navigation Control -->
+    <v-navigation-drawer
+      app
+      clipped
+      stateless
+      hide-overlay
+      :mini-variant="$vuetify.breakpoint.name === 'sm' || 
+      $vuetify.breakpoint.name === 'xs' ? true : false"
+      v-model="drawer"
+      width='260'>
+      <menu_item/>
+    </v-navigation-drawer>
+    <!-- End - Navigation Control -->
+
+    <v-toolbar app fixed clipped-left>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
+        <span>DHTML</span>
+        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn
+      <!-- <v-btn
         flat
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
       >
         <span class="mr-2">Latest Release</span>
-      </v-btn>
+      </v-btn> -->
+      <v-btn icon @click.stop="drawer = !drawer">
+        <v-icon>menu</v-icon>
+        </v-btn>
     </v-toolbar>
 
     <v-content>
-      <HelloWorld/>
+      <router-view/>
+      <!-- <HelloWorld/> -->
     </v-content>
+    <v-footer app>
+    <v-layout justify-center row wrap>
+      <!-- <p>{{$t("footer.copyright")}}</p> -->
+      <p>Made in <i class="fa fa-users" aria-hidden="true"></i> by ANALITICOM</p>
+    </v-layout>
+  </v-footer>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+// import HelloWorld from './components/HelloWorld'
+import menu from './components/navigation/Menu.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+     menu_item:menu,
   },
   data () {
     return {
-      //
+      drawer: null,
     }
   }
 }
---
+++ src/components/navigation/Menu.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <v-list-tile>
+      <v-list-tile-action>
+        <router-link to="/">
+            <v-icon>home</v-icon>
+        </router-link>
+      </v-list-tile-action>
+      <v-list-tile-content>
+        <v-list-tile-title>
+          <router-link to="/">
+            <v-list-tile-title>
+              Home
+            </v-list-tile-title>
+          </router-link>  
+        </v-list-tile-title>
+      </v-list-tile-content>
+    </v-list-tile>
+  </div>
+</template>
