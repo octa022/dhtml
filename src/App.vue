c5f6466d97b33116c27d5a813d1a6ff4aca61de6
Octavio Torres <octa022@gmail.com>
2019-01-09 02:09:55 -0400

    Estructura Base, Cambio del home

--- src/App.vue
+++ src/App.vue
@@ -9,39 +9,45 @@
       :mini-variant="$vuetify.breakpoint.name === 'sm' || 
       $vuetify.breakpoint.name === 'xs' ? true : false"
       v-model="drawer"
-      width='260'>
+      width='220'>
       <menu_item/>
     </v-navigation-drawer>
     <!-- End - Navigation Control -->
-
-    <v-toolbar app fixed clipped-left>
+    
+    <!-- Nav -->
+    <v-toolbar app fixed clipped-left class="grey darken-2                                         white--text">
+      <v-btn flat icon to='/' outline color="transparent">
+        <v-avatar 
+          :size="$vuetify.breakpoint.name === 'sm' || $vuetify.breakpoint.name === 'xs' ? 36 : 50" color="grey lighten-3"
+        >
+          <img src="../public/analiticom.jpg" alt="avatar">
+        </v-avatar>
+      </v-btn>
+      <v-spacer></v-spacer>
       <v-toolbar-title class="headline text-uppercase">
         <span>DHTML</span>
-        <!-- <span class="font-weight-light">MATERIAL DESIGN</span> -->
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <!-- <v-btn
-        flat
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn> -->
       <v-btn icon @click.stop="drawer = !drawer">
-        <v-icon>menu</v-icon>
+        <v-icon class="white--text">menu</v-icon>
         </v-btn>
     </v-toolbar>
-
+    <!-- End - Nav -->
+    
+    <!-- Content -->
     <v-content>
       <router-view/>
       <!-- <HelloWorld/> -->
     </v-content>
-    <v-footer app>
-    <v-layout justify-center row wrap>
-      <!-- <p>{{$t("footer.copyright")}}</p> -->
-      <p>Made in <i class="fa fa-users" aria-hidden="true"></i> by ANALITICOM</p>
-    </v-layout>
+    <!-- End - Content -->
+    
+    <!-- Footer -->
+    <v-footer class="grey darken-2 white--text">
+      <v-layout justify-center row wrap>
+        <span class="caption">Anatilicom - &copy; 2019</span>
+      </v-layout>
   </v-footer>
+  <!-- End - Footer -->
   </v-app>
 </template>
 
--- src/components/navigation/Menu.vue
+++ src/components/navigation/Menu.vue
@@ -1,18 +1,18 @@
 <template>
-  <div>
-    <v-list-tile>
+  <div class="menu-list">
+    <v-list-tile to="/">
       <v-list-tile-action>
-        <router-link to="/">
-            <v-icon>home</v-icon>
-        </router-link>
+        <!-- <router-link to="/"> -->
+          <v-icon>home</v-icon>
+        <!-- </router-link> -->
       </v-list-tile-action>
       <v-list-tile-content>
         <v-list-tile-title>
-          <router-link to="/">
+          <!-- <router-link to="/"> -->
             <v-list-tile-title>
               Home
             </v-list-tile-title>
-          </router-link>  
+          <!-- </router-link>   -->
         </v-list-tile-title>
       </v-list-tile-content>
     </v-list-tile>
