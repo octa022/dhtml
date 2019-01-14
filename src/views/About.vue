f13bc33abedfd4e93eb01fd389ea6f7a189f712a
guttymora <tavo.mora92@hotmail.com>
2019-01-14 18:03:07 -0400

    Informacion del tema

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>DHTML</h1>
+    <p>
+      Es un término que se usó a fines de los años 90 y principios de los 2000 para describir cualquier tipo de funcionalidad 
+      que de alguna manera fuera del ámbito de la navegación regular de sitios web. En realidad, solo era una palabra de moda 
+      difícil de definir, como la Web 2.0.
+    </p>
+    <p>
+      La definición de HTML dinámico o DHTML, podría ser que es "HTML que puede cambiar incluso después de que una página
+       se haya cargado en un navegador".
+    </p>
+    <p>
+      las últimas tendencias en el desarrollo de aplicaciones web están más relacionadas con DHTML. En la antiguedad, 
+      los diseños div eran nuevos y JavaScript se convirtió casi en una maldición. Era imperativo que el sitio siguiera 
+      siendo utilizable sin JavaScript.
+    </p>
+    <p>
+      Desde entonces, se han producido múltiples oleadas de mejoras en las tecnologías web front-end, como CSS3, HTML5, jQuery.
+       En 2004, Gmail y AJAX introdujeron un nuevo nivel de expectativa para las aplicaciones web a la corriente principal, 
+       seguido de Google Maps un año después. Todo esto lleva a las aplicaciones web ricas que tenemos hoy.
+    </p>
+    <p>
+      Las implementaciones del navegador de HTML y CSS son hoy en día muy buenas y los proveedores de navegadores se 
+      enorgullecen de ofrecer la máxima ejecución de JavaScript. Lo que surgió de la era de los estándares web fue la 
+      separación de contenido, diseño y lógica con HTML, CSS y JavaScript.
+    </p>
+    <p>
+      Las tendencias indican la fusion de todo, junto con el paquete de módulos, Webpack React o Vue.js se está utilizando para 
+      crear código que se asemeja al código DHTML de 1999 más que el código basado en jQuery de 2009. Los componentes web 
+      estándar se están demorando, pero (React o Vue) + Webpack ya le permite trabajar con HTML, JS, CSS y Imágenes todo 
+      en el mismo entorno. En DHTML hiciste todo con JavaScript, porque podías, hoy está hecho porque tiene sentido.
+    </p>
+    <p>
+      Las tendencias indican la fusion de todo, crear código que se asemeja al código DHTML de 1999 
+      más que el código basado en jQuery de 2009.
+    </p>
+    <p>
+      También hay una tendencia a desalentar el uso de CSS en los componentes React. Esto no significa que se 
+      eliminaría el estilo, sino que se eliminaría el uso de hojas de estilo en cascada con clases e identificadores. 
+      Esto suena como una desviación bastante brusca de las prácticas anti-en línea de facto que hemos tenido desde la 
+      popularización de CSS.
+    </p>
+    <p>
+      En lugar de alternar la vista de sus componentes con clases (definidas en un CSS creado con SASS / LESS), 
+      modificará los estilos de los elementos directamente. Las clases de hover y otros efectos similares se realizarán a 
+      través de eventos de JavaScript, no pseudo selectores como: hover.
+    </p>
+    <p>
+      Las hojas de estilo en cascada obviamente tienen sus usos en la construcción de sitios, pero para los 
+      componentes individuales. Incluso si acostumbrarse a la idea lleva un tiempo, computar los estilos de elementos en 
+      el navegador es tan poderoso como siempre lo ha sido.
+    </p>
+    <h1>Regreso al futuro: un desplazador DHTML desde 2001</h1>
   </div>
 </template>
