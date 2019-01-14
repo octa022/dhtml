717bf3cc4fbe50576952596b6f2018c47c40b6e6
Octavio Torres <octa022@gmail.com>
2019-01-14 13:23:17 -0400

    Se Crearon Botones de Herramientas.

--- src/components/navigation/ToolsPanel.vue
+++ src/components/navigation/ToolsPanel.vue
@@ -1,9 +1,29 @@
 <template>
-  <div>
-    <h3 class="text-sm-center">Panel de Herramientas</h3>
-
-      <v-btn color="success" @click="$emit('addContent')" key="btn-2" class="ma-5">Agregar</v-btn>
-
-      <v-btn class="sec2">Sec 2</v-btn>
-  </div>
+  <v-layout row wrap class="text-xs-center">
+    <v-flex xs12>
+      <h3 class="text-sm-center">Panel de Herramientas</h3>
+    </v-flex>
+    <v-flex xs12 sm6>
+      <v-btn class="sec2">
+        <v-icon>fab fa-windows</v-icon>
+      </v-btn>
+    </v-flex>
+    <v-flex xs12 sm6>
+      <v-btn class="sec3">
+        <div v-for="item in 2" :key="item">
+          <div v-for="item in 2" :key="item"></div>
+        </div>
+      </v-btn>
+    </v-flex>
+    <v-flex xs12 sm6>
+      <v-btn class="sec10">
+        <div v-for="item in 2" :key="item">
+          <div v-for="item in 2" :key="item"></div>
+        </div>
+      </v-btn>
+    </v-flex>
+     <v-flex xs12 sm6>
+      <v-btn class="sec13"></v-btn>
+    </v-flex>
+  </v-layout>
 </template>
