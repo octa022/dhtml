45154f3e4048cc1b2bf5aff5acae96c03c9d51a9
Octavio Torres <octa022@gmail.com>
2019-01-14 18:12:09 -0400

    Boton Flotante

--- src/components/uieditor/container.vue
+++ src/components/uieditor/container.vue
@@ -1,6 +1,18 @@
 <template>
 <v-container>
   <uieditor></uieditor>
+   <v-btn
+      v-if="$route.name === 'create'" @click.prevent="$emit('editPanel')"
+      v-show="!hidden"
+      color="pink"
+      dark
+      absolute
+      bottom
+      right
+      fab
+    >
+      <v-icon>fa-edit</v-icon>
+    </v-btn>
 </v-container>
 </template>
 <script src="./container.js"></script>
