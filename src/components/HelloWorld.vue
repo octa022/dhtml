88725b7f74dc61c4f4d8fb82024c848b8b527ddb
guttymora <tavo.mora92@hotmail.com>
2019-01-14 12:11:35 -0400

    Pruebas vacias

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,4 @@
 <template>
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex mb-4>
-        <h1 class="main-title">DHTML</h1>
-        <p class="subheading font-weight-regular font-zcool">
-          HTML Dinámico
-        </p>
-      </v-flex>
-
-    </v-layout>
-  </v-container>
+    <div >Hola</div>
 </template>
 
-<script>
-  export default {
-    data(){
-      return {
-        //
-      }
-    }
-  }
-</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,11 @@
 <template>
-  <!-- <HelloWorld /> -->
-  <v-container>
-    <v-layout
-      text-xs-center
-      wrap
-    >
-      <v-flex mb-4>
-        <h1 class="main-title">DHTML</h1>
-        <p class="subheading font-weight-regular font-zcool">
-          HTML Dinámico
-        </p>
-      </v-flex>
-
-    </v-layout>
-  </v-container>
+  <hello-world></hello-world>
 </template>
-
 <script>
+  import HelloWorld from '../components/HelloWorld.vue'
   export default {
+    components: {
+      HelloWorld
+    }
   }
 </script>
