8f861aedafb4e35b48215adf4db1aa02b563ea6d
julian141094 <julian.arkangel@gmail.com>
2019-01-14 17:14:16 -0400

    Cambios en componentes prefabricados

--- src/components/layout/LayoutSec10.vue
+++ src/components/layout/LayoutSec10.vue
@@ -1,12 +1,12 @@
 <template>
   <v-container  grid-list-md text-xs-center>
     <v-layout row wrap>
-      <v-flex xs6 style="border: 1px solid black" pa-4>
+      <v-flex xs4 style="border: 1px solid black" pa-4>
         <v-flex xs12 style="border: 1px blue solid; background-color: red;" pa-4>
           secuencia 11 
         </v-flex>
       </v-flex>
-      <v-flex xs6 style="border: 1px solid black" pa-4>
+      <v-flex xs8 style="border: 1px solid black" pa-4>
         <v-flex xs12 style="border: 1px blue solid; background-color: blue;" pa-4>
           secuencia 12 
         </v-flex>
