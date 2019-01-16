4c6cf145dc9146e80ce89f68602a8c9a2fa2af9b
julian141094 <julian.arkangel@gmail.com>
2019-01-16 09:22:20 -0400

    Modificacion del ejemplo de drag an drop

--- src/components/example/ExampleThree.vue
+++ src/components/example/ExampleThree.vue
@@ -1,16 +1,19 @@
 <template>
   <v-container grid-list-xs>
     <v-layout row wrap>
-      <v-flex xs12>
+      <v-flex lg12>
         <h1>Arrastra y Suelta los elementos de abajo</h1> <br><br>
-          <draggable :list="list.upvotes">
-            <transition-group>
-              <div v-for="element in list" :key="element">
-                      <h2>{{element}}</h2>
-              </div>
-            </transition-group>
-        </draggable>
-      </v-flex> 
+        <h1>Lista</h1> <br><br>
+        <v-flex lg4>
+            <draggable class="drag1" :list="list.upvotes">
+              <transition-group>
+                <div class="elementList" v-for="element in list" :key="element">
+                        <h2>{{element}}</h2>
+                </div>
+              </transition-group>
+            </draggable>
+        </v-flex> 
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -32,3 +35,16 @@ export default {
   }
 }
 </script>
+<style>
+.drag1{
+  background-color: grey;
+  
+}
+.elementList{
+  padding: 4px;
+  margin-top: 4px;
+  border: solid 1px;
+  transition: all 0,5s;
+  background-color: white;
+}
+</style>
