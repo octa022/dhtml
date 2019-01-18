337b50a3cea154dc516ddbb02acca4a1829c2f35
Aberbin96 <alejandroberbin@gmail.com>
2019-01-18 09:41:25 -0400

    Ultimo retoque

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,30 @@
 <template>
-  <hello-world></hello-world>
+  <!-- <HelloWorld /> -->
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+      <v-flex mb-4>
+        <h1 class="main-title">DHTML</h1>
+        <p class="subheading font-weight-regular font-zcool">
+          HTML Dinámico
+        </p>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
 </template>
+
 <script>
-  import HelloWorld from '../components/HelloWorld.vue'
   export default {
-    components: {
-      HelloWorld
-    }
+  data () {
+      return {
+         
+      }
+  },
+  components: {
+    
+  }
   }
 </script>
