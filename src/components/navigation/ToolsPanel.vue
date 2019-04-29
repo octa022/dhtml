6778178f5db25a45c9df436310ab7ad9787e448d
julian141094 <julian.arkangel@gmail.com>
2019-04-29 14:53:25 -0400

    creacion de modal

--- src/components/navigation/ToolsPanel.vue
+++ src/components/navigation/ToolsPanel.vue
@@ -55,19 +55,15 @@
         <u>Inputs</u>
       </h4>
     </v-flex>
-    <v-flex xs12 sm6 @click="createFooter(1)">
-      <v-btn class="sec13"></v-btn>
-      <v-dialog v-model="dialog" persistent max-width="290">
-        <template v-slot:activator="{ on }">
-          <v-btn color="primary" dark v-on="on">Open Dialog</v-btn>
-        </template>
+    <v-flex xs12 sm6>
+      <v-btn class="sec13" @click.prevent="dialogConfig=true"></v-btn>
+      <v-dialog v-model="dialogConfig" persistent max-width="80%">
         <v-card>
-          <v-card-title class="headline">Use Google's location service?</v-card-title>
-          <v-card-text>Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.</v-card-text>
+          <modalConf ref="modalConf"/>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn color="green darken-1" flat @click="dialog = false">Disagree</v-btn>
-            <v-btn color="green darken-1" flat @click="dialog = false">Agree</v-btn>
+            <v-btn color="green darken-1" flat @click="dialogConfig = false">Disagree</v-btn>
+            <v-btn color="green darken-1" flat @click="dialogConfig = false">Agree</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -75,6 +71,7 @@
   </v-layout>
 </template>
 <script>
+import modalConf from "../inputs/modalConfig/ModalConfig.vue";
 export default {
   // {
   //   props:{
@@ -83,6 +80,14 @@ export default {
   //   },
   //   key: elementkey
   // }
+  components: {
+    modalConf: modalConf
+  },
+  data() {
+    return {
+      dialogConfig: false
+    };
+  },
   methods: {
     createRow(number) {
       this.$emit("createElement", {
