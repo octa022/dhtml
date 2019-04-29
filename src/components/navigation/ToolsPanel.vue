7d13980cd440f327a57b4e37d35ca35d5a401ed4
Octavio Torres <octa022@gmail.com>
2019-04-29 11:11:57 -0400

    Modificaciones en el menu de herramientas

--- src/components/navigation/ToolsPanel.vue
+++ src/components/navigation/ToolsPanel.vue
@@ -1,19 +1,17 @@
 <template>
-<v-layout row wrap class="text-xs-center">
+<v-layout row wrap class="text-xs-center tools-panel">
   <v-flex xs12>
     <h3 class="text-sm-center text-uppercase">Panel de Herramientas</h3>
   </v-flex>
-  <v-flex pt-3 xs12>
+  <!-- <v-flex pt-3 xs12>
     <h4 class="text-sm-center"><u>Toolbars</u></h4>
   </v-flex>
   <v-flex xs12 sm6>
-    <!-- Cada boton emite el componente que desea crear -->
     <v-btn class="sec2" @click="createToolBar(1)">
       <v-icon>fab fa-windows</v-icon>
     </v-btn>
   </v-flex>
   <v-flex xs12 sm6>
-    <!-- Cada boton emite el componente que desea crear -->
     <v-btn class="sec2" @click="createSubToolBar(1)">
       <v-icon>fab fa-windows</v-icon>
     </v-btn>
@@ -43,7 +41,65 @@
   </v-flex>
   <v-flex xs12 sm6 @click="createFooter(1)">
     <v-btn class="sec13"></v-btn>
+  </v-flex> -->
+
+  <v-flex pt-3 xs12>
+    <v-expansion-panel>
+      <v-expansion-panel-content>
+        <div slot="header" class="font-weight-medium text-uppercase">Toolbars</div>
+        <v-flex xs12 sm6>
+          <!-- Cada boton emite el componente que desea crear -->
+          <v-btn class="sec2" @click="createToolBar(1)">
+            <v-icon>fab fa-windows</v-icon>
+          </v-btn>
+        </v-flex>
+        <v-flex xs12 sm6>
+          <!-- Cada boton emite el componente que desea crear -->
+          <v-btn class="sec2" @click="createSubToolBar(1)">
+            <!-- <v-icon>fab fa-windows</v-icon> -->
+          </v-btn>
+        </v-flex>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
   </v-flex>
+
+  <v-flex pt-3 xs12>
+    <v-expansion-panel>
+      <v-expansion-panel-content>
+        <div slot="header" class="font-weight-medium text-uppercase">Layout</div>
+        <v-flex xs12 sm6>
+          <v-btn class="sec3" @click="createRow(2)">
+            <div v-for="item in 2" :key="item">
+              <div v-for="item in 2" :key="item"></div>
+            </div>
+          </v-btn>
+        </v-flex>
+        <v-flex xs12 sm6 @click="createRow(3)">
+          <v-btn class="sec10">
+            <div v-for="item in 2" :key="item">
+              <div v-for="item in 2" :key="item"></div>
+            </div>
+          </v-btn>
+        </v-flex>
+         <v-flex xs12 sm6 @click="createRow(1)">
+          <v-btn class="sec13"></v-btn>
+        </v-flex>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-flex>
+
+   <v-flex pt-3 xs12>
+    <v-expansion-panel>
+      <v-expansion-panel-content>
+        <div slot="header" class="font-weight-medium text-uppercase">Footer</div>
+        <v-flex xs12 sm6 @click="createFooter(1)">
+          <v-btn class="sec13"></v-btn>
+        </v-flex>
+      </v-expansion-panel-content>
+    </v-expansion-panel>
+  </v-flex>
+
+
 </v-layout>
 </template>
 <script>
@@ -99,3 +155,11 @@ export default{
   } 
 }
 </script>
+
+<style>
+  .tools-panel .v-expansion-panel__body {
+    display: flex;
+    flex-wrap: wrap;
+    flex: 1 1 auto;
+  }
+</style>
