42878a3c43b7beeaedd511687112057cc7a4f1c7
julian141094 <julian.arkangel@gmail.com>
2019-05-29 09:51:44 -0400

    Pruebas de miguel

--- src/components/uieditor/uieditor.vue
+++ src/components/uieditor/uieditor.vue
@@ -1,32 +1,73 @@
 <script>
 /* eslint-disable */
-import toolbar from "../tools/Toolbar.vue"
-import subtoolbar from "../tools/SubToolbar.vue"
-import layout from "../layout/Layout.vue"
-import footer from "../tools/Footer.vue"
-import formfields from "../inputs/TextInputBrick.vue"
+import toolbar from "../tools/Toolbar.vue";
+import subtoolbar from "../tools/SubToolbar.vue";
+import layout from "../layout/Layout.vue";
+import footer from "../tools/Footer.vue";
+import formfields from "../inputs/TextInputBrick.vue";
 // Asi se importan las marcas de Vuetify
-import * as comp from 'vuetify/lib/components'
-import draggable from 'vuedraggable'
+import * as comp from "vuetify/lib/components";
+import draggable from "vuedraggable";
 
 let listComponents = {
-  "layout":[layout,0],
-  "toolbar":[toolbar,0],
-  "subtoolbar":[subtoolbar,0],
-  "footer":[footer,0],
-  "formfields":[formfields,0]
-}
+  layout: [layout, 0],
+  toolbar: [toolbar, 0],
+  subtoolbar: [subtoolbar, 0],
+  footer: [footer, 0],
+  formfields: [formfields, 0]
+};
 
 export default {
-  data(){
+  data() {
     return {
-      comps:[
+      comps: [
+        {
+          tag: layout,
+          options: {
+            key: "layout_1",
+            props: {
+              idname: "layout_1",
+              mode: 3
+            }
+          }
+        },
+        {
+          tag: "div",
+          options: {
+            style: {
+              border: "31px solid yellow"
+            },
+            key: "div_2"
+          },
+          parent: "layout_1"
+        },
+        {
+          tag: "div",
+          options: {
+            style: {
+              border: "31px solid blue"
+            },
+            key: "div_1"
+          },
+          parent: "layout_1"
+        },
+        {
+          tag: "div",
+          options: {
+            style: {
+              border: "31px solid red"
+            },
+            key: "div_3",
+            slot: "greenwii"
+          },
+          parent: "layout_1"
+        }
         // {
         //   tag: formfields,
         //   options:{
         //     props:{
         //       brickJSON:{
-        //         "fieldCode" : "field1", 
+        //         "fieldCode" : "field1",
         //         "fieldValue": "",
         //         "fieldWidth":"xs12 md8",
         //         "fieldColor":"blue-grey",
@@ -35,8 +76,8 @@ export default {
         //         "label" : "Chavista sin sentido",
         //         "labelPosition" : "left",
         //         "append":"",
-        //         "prepend":"fa-tasks",  
-        //         "defaultValue" : "",  
+        //         "prepend":"fa-tasks",
+        //         "defaultValue" : "",
         //         "brickType" : "datepicker",
         //         "visible" : true,
         //         "prefix":"das",
@@ -56,7 +97,7 @@ export default {
         //   options:{
         //     props:{
         //       brickJSON:{
-        //         "fieldCode" : "field2", 
+        //         "fieldCode" : "field2",
         //         "fieldValue": "",
         //         "fieldWidth":"xs12 md6",
         //         "fieldColor":"green",
@@ -65,8 +106,8 @@ export default {
         //         "label" : "Prueba",
         //         "labelPosition" : "top",
         //         "append-icon":"",
-        //         "prepend-icon":"",  
-        //         "defaultValue" : "",  
+        //         "prepend-icon":"",
+        //         "defaultValue" : "",
         //         "brickType" : "TEXTINPUT",
         //         "visible" : true,
         //         "prefix":"xs",
@@ -166,53 +207,60 @@ export default {
         //   parent:'layout_3'
         // }
       ]
-    }
+    };
   },
-  render(createElement){
-    var addchildrens = (children) => {
-      return children.map((node) => {
-        let childrens = this.comps.filter(i => i.parent === node.options.key) 
-        return createElement(node.tag,
+  render(createElement) {
+    var addchildrens = children => {
+      return children.map(node => {
+        let childrens = this.comps.filter(i => i.parent === node.options.key);
+        return createElement(
+          node.tag,
           JSON.parse(JSON.stringify(node.options)),
-          childrens.length > 0 ? addchildrens(childrens):''
-        )
+          childrens.length > 0 ? addchildrens(childrens) : ""
+        );
+      });
+    };
+    return createElement(
+      draggable,
+      this.comps.map(val => {
+        if (val.parent == undefined || val.parent == "") {
+          let childrens = this.comps.filter(i => i.parent === val.options.key);
+          return createElement(
+            val.tag,
+            JSON.parse(JSON.stringify(val.options)),
+            childrens.length > 0 ? addchildrens(childrens) : ""
+          );
+        }
       })
-    }
-    return createElement(draggable,this.comps.map((val)=>{
-      if(val.parent == undefined || val.parent == ""){
-        let childrens = this.comps.filter(i => i.parent === val.options.key)
-        return createElement(val.tag, JSON.parse(JSON.stringify(val.options)),
-          childrens.length > 0 ? addchildrens(childrens):''
-        )
-      }
-    }))
+    );
   },
-  methods:{
-    addElement(evt){
+  methods: {
+    addElement(evt) {
       // let listComponents = {
       //   "layout":[layout,0],
       //   "toolbar":[toolbar,0],
       //   "subtoolbar":[subtoolbar,0],
       //   "footer":[footer,0]
       // }
-      listComponents[evt.tag][1] += 1
+      listComponents[evt.tag][1] += 1;
       //evt.tag+'_'+listComponents[evt.tag][1]
       //example layout_1
-      let elementkey = `${evt.tag}_${listComponents[evt.tag][1]}` 
-      let instanceVue = listComponents[evt.tag][0]
+      let elementkey = `${evt.tag}_${listComponents[evt.tag][1]}`;
+      let instanceVue = listComponents[evt.tag][0];
       //console.log("key",elementkey)
-      evt.options.key = elementkey
-      evt.options.props.idname = elementkey
+      evt.options.key = elementkey;
+      evt.options.props.idname = elementkey;
       this.comps.push({
-        tag:instanceVue,
-        options:evt.options,
+        tag: instanceVue,
+        options: evt.options,
         parent: evt.parent === undefined ? "" : evt.parent
-      })
+      });
+      console.log(this.comps, "comps");
     },
-    removeElement(evt){
-      let i = this.comps.findIndex(j => j.options.key === evt.keyname)
-      this.comps.splice(i,1)
+    removeElement(evt) {
+      let i = this.comps.findIndex(j => j.options.key === evt.keyname);
+      this.comps.splice(i, 1);
     }
   }
-}
+};
 </script>
